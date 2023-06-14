b1b144aa37c01731bfdfe456e4f8487307354e65
Laurentiustd <laurenttius117@gmail.com>
2023-06-14 17:29:59 +0700

    add endpoint api & authorization

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Http\Resources\BookResources;
+use App\Models\Book;
+use App\Models\User;
+use Auth;
+
+class ApiController extends Controller
+{
+    public function index(){
+        $book = Book::all();
+        return BookResources::collection($book);
+        // return response()->json(['data'=>$book]);
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ], [
+            'email.required' => 'Email must be filled',
+            'password.required' => 'Password must be filled',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+    }
+
+    public function logout(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ], [
+            'email.required' => 'Email must be filled',
+            'password.required' => 'Password must be filled',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        $user->tokens()->delete();
+
+        return 'logout succesfully';
+    }
+
+    public function create(Request $request){
+        $book = Book::create([
+            'Title' => $request -> title,
+            'Author' => $request -> author,
+            'ReleaseDate' => $request ->releaseDate,
+            'Quantity' => $request -> quantity,
+            'Price' => $request -> price,
+            'Image' => "image",
+            'category_id' => "1"
+        ]);
+        return response()->json(["messages"=>"success","data"=>$book, "created_by" => Auth::user()]);
+    }
+
+    public function update(Request $request, $id){
+        $book = Book::findOrFail($id)->update([
+            'Title' => $request -> title,
+            'Author' => $request -> author,
+            'ReleaseDate' => $request ->releaseDate,
+            'Quantity' => $request -> quantity,
+            'Price' => $request -> price,
+            'Image' => "image",
+            'category_id' => "1"
+        ]);
+        return response()->json(["messages"=>"success","data"=>$book, "updated_by" => Auth::user()]);
+    }
+
+    public function delete($id){
+        $book = Book::find($id);
+        Book::destroy($book);
+        return response()->json(["messages"=>"success","data"=>$book, "deleted_by" => Auth::user()]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
-
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/books', [ApiController::class, 'index']);
+
+Route::post('/loginapi', [ApiController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logoutapi', [ApiController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/createapi', [ApiController::class, 'create']);
+Route::middleware('auth:sanctum')->post('/updateapi/{id}', [ApiController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/deleteapi/{id}', [ApiController::class, 'delete']);
